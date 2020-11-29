c43362977f676491243c693a09e6721f7b4a0f03
nibmz7 <nibmz7@gmail.com>
2020-11-29 18:01:47 +0800

    Fixed array string parser for multi digit numbers

--- array_string_parser/index.js
+++ array_string_parser/index.js
@@ -1,17 +1,18 @@
 console.log(parseArray("[]"));
 console.log(parseArray("[1, 2, 3]"));
-console.log(parseArray("[1, [2, [3, 7, ], 4], 5]"));
-console.log(parseArray("[1, [2, [3, 4], 4], 5, [8, 0, 1]]"));
+console.log(parseArray("[1, [2, [3, 7], 4], 5]"));
+console.log(parseArray("[1, [2, [34, 4], 4], 5, [8, 0, 1]]"));
 
 function parseArray(str) {
   const START_BRACKET = "[";
   const CLOSE_BRACKET = "]";
-  const input = str.replace(/\s/g, "").slice(1, -1);
+  const input = str.replace(/\s/g, "").slice(1);
 
   const result = [];
 
   let openedBrackets = 0;
   let subArrayString = "";
+  let currentItem = "";
 
   for (const char of input) {
     if (char === START_BRACKET) openedBrackets++;
@@ -22,12 +23,21 @@ function parseArray(str) {
         if (openedBrackets === 0) {
           const subArray = parseArray(subArrayString);
           result.push(subArray);
-          subArrayString = '';
+          subArrayString = "";
         }
       }
-    } else if (char !== ",") {
-      result.push(Number(char));
+      continue;
+    }
+    if (char === "," || char === CLOSE_BRACKET) {
+      if (currentItem.length >= 1) {
+        result.push(Number(currentItem));
+        currentItem = "";
+      }
+    } else {
+      currentItem += char;
     }
   }
   return result;
 }
+
+
