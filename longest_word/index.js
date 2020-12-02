028c48f8350965a3926f9c54c429f5b948ddeab4
nibmz7 <nibmz7@gmail.com>
2020-12-02 21:03:28 +0800

    Longest word through deleting

--- longest_word/index.js
+++ longest_word/index.js
@@ -1,7 +1,7 @@
-console.log(longestWord(["w", "wo", "wor", "worl", "world"]));
-console.log(
-  longestWord(["a", "banana", "app", "appl", "ap", "apply", "apple"])
-);
+// console.log(longestWord(["w", "wo", "wor", "worl", "world"]));
+// console.log(
+//   longestWord(["a", "banana", "app", "appl", "ap", "apply", "apple"])
+// );
 
 function longestWord(words) {
   words.sort();
@@ -21,3 +21,28 @@ function longestWord(words) {
 
   return result;
 }
+//Longest Word in Dictionary through Deleting
+function findLongestWord(searchWord, dictionary) {
+  if (searchWord.length === 0) return searchWord;
+  dictionary.sort();
+  const maxLength = searchWord.length;
+  let result = "";
+  wordLoop: for (const word of dictionary) {
+    let currentIndex = 0;
+    let wordLength = word.length;
+    for (const char of word) {
+      while (char !== searchWord[currentIndex]) {
+        currentIndex++;
+        if (maxLength - currentIndex < wordLength) continue wordLoop;
+        if (currentIndex === maxLength) continue wordLoop;
+      }
+      wordLength--;
+      currentIndex++;
+    }
+    result = word.length > result.length ? word : result;
+  }
+  return result;
+}
+
+console.log(findLongestWord("abpcplea", ["ale", "apple", "monkey", "plea"]));
+console.log(findLongestWord("", ["b", "a"]));
